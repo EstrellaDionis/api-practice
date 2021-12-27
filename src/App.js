cc6dd0358c5e39ad2a2eb949d8aeb2633130516c
EstrellaDionis <EstrellaDionis@gmail.com>
2021-12-26 23:40:35 -0500

    used axios to get info from dad-jokes api and have info displayed in console log

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
 //This is the "promise-y version"
-fetch('https:///api.cryptonator.com/api/ticker/btc-usd')
-  .then(res => {
-    console.log("RESPONSE, WAITING TO PARSE", res)
-    return res.json()
-  })
-  .then(data => {
-    console.log("DATA PARSED", data)
-    console.log(data.ticker.price)
-  })
-  .catch(err => {
-    console.log("ERROR", err)
-  })
+// fetch('https:///api.cryptonator.com/api/ticker/btc-usd')
+//   .then(res => {
+//     console.log("RESPONSE, WAITING TO PARSE", res)
+//     return res.json()
+//   })
+//   .then(data => {
+//     console.log("DATA PARSED", data)
+//     console.log(data.ticker.price)
+//   })
+//   .catch(err => {
+//     console.log("ERROR", err)
+//   })
 
 //this is the async version
 // const fetchBitcoinPrice = async () => {
@@ -23,6 +24,39 @@ fetch('https:///api.cryptonator.com/api/ticker/btc-usd')
 //   console.log(data.ticker.price)
 // }
 
+//axios
+axios.get('https:///api.cryptonator.com/api/ticker/btc-usd')
+  .then(res => {
+    console.log(res.data.ticker.price)
+  })
+  .catch(err => {
+    console.log("Error!", err)
+  })
+
+// const fetchBitcoinPrice = async () => {
+//   try {
+//     const res = await axios.get('https:///api.cryptonator.com/api/ticker/btc-usd')
+//     console.log(res.data.ticker.price)
+//   }catch(e){
+//     console.log("ERROR!", e)
+//   }
+// }
+
+
+axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}})
+  .then(res => {
+    console.log(res.data.joke)
+  })
+  .catch(err => {
+    console.log("Error!", err)
+  })
+
+// const getDadJoke = async () => {
+//   const config = { headers: {Accept: 'application/json'}}
+//   const res = await axios.get('https://icanhazdadjoke.com/')
+//   console.log(res)
+//   return res.data.joke;
+// }
 
 function App() {
   return (
